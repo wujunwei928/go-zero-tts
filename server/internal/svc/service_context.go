bf25718b47441c7c4997ead53eb4310acc0a6e82
wujunwei <1399952803@qq.com>
2024-04-19 17:28:44 +0800

    init; backend use go-zero; frontend use vuetify

---
+++ server/internal/svc/service_context.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"server/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
